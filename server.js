083e028a0c44ff7d2cc73a8f23733b7080288417
mukesh <mukese0794@gmail.com>
2023-07-23 16:37:14 +0530

    cors

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import cors from "cors";
 const app = express();
 dotenv.config();
 
 import ItemRoutes from "./routers/items.js"
 
+app.use(cors());
 app.use(express.json());
 mongoose.connect(process.env.MONGO_URI);
 
